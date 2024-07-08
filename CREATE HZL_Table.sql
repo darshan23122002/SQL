6d0c230e0646739d97b1e98795effec45cfa9013
Darshan Bapusaheb Tipale <114793075+darshan23122002@users.noreply.github.com>
2024-07-08 01:45:20 -0700

    Add files via upload

---
+++ CREATE HZL_Table.sql
@@ -0,0 +1,3 @@
+USE master;
+
+SELECT * FROM HZL_Table;
---
+++ Repalcing Null values.sql
@@ -0,0 +1,27 @@
+USE master;
+
+WITH CTE AS (
+    SELECT
+        [Date],
+        [BU],
+        [Value],
+        ROW_NUMBER() OVER (PARTITION BY [BU] ORDER BY [Date]) AS rn
+    FROM HZL_Table
+)
+SELECT 
+    c1.[Date],
+    c1.[BU],
+    COALESCE(c1.[Value], c2.[Value]) AS [Value]
+FROM 
+    CTE c1
+OUTER APPLY (
+    SELECT TOP 1 [Value]
+    FROM CTE c2
+    WHERE c2.[BU] = c1.[BU] 
+      AND c2.rn < c1.rn
+      AND c2.[Value] IS NOT NULL
+    ORDER BY c2.rn DESC
+) c2
+ORDER BY 
+    c1.[BU], 
+    c1.[Date];
